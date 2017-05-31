f62c88c68db5228aa1bfe73c5e0553c244c1cbd0
Krzysztof Maziarz <krzysztof.s.maziarz@gmail.com>
2017-06-01 00:05:40 +0200

    Add vertex cover in bipartite graphs algorithm

--- BipartiteGraph.cpp
+++ BipartiteGraph.cpp
@@ -1,8 +1,12 @@
 #include "BipartiteGraph.h"
 
-BipartiteGraph::BipartiteGraph(std::size_t n): n(n), graph(n) {}
+BipartiteGraph::BipartiteGraph(std::size_t n, std::vector<bool>& side): n(n), graph(n), side(side) {
+    assert(side.size() == n);
+}
 
 void BipartiteGraph::addEdge(int u, int v) {
+    assert(side[u] != side[v]);
+
     graph[u].push_back(v);
     graph[v].push_back(u);
 }
@@ -43,4 +47,39 @@ std::vector<int> BipartiteGraph::findMatching() {
     }
 
     return mate;
+}
+
+std::vector<bool> BipartiteGraph::findVertexCover() {
+    auto mate = findMatching();
+
+    std::vector<bool> visited(n, false);
+    std::queue <int> q;
+    for (int i = 0; i < n; i++) {
+        if (side[i] && mate[i] == -1) {
+            visited[i] = true;
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (int v : graph[u]) if (!visited[v] && v != mate[u]) {
+                visited[v] = true;
+
+                int x = mate[v];
+                if (x != -1 && !visited[x]) {
+                    visited[x] = true;
+                    q.push(x);
+                }
+            }
+    }
+
+    std::vector<bool> inCover(n, false);
+    for (int i = 0; i < n; i++) {
+        inCover[i] = visited[i] xor side[i];
+    }
+
+    return inCover;
 }
--- BipartiteGraph.h
+++ BipartiteGraph.h
@@ -1,19 +1,23 @@
 #ifndef VERTEX_COVER_BIPARTITEGRAPH_H
 #define VERTEX_COVER_BIPARTITEGRAPH_H
 
+#include <cassert>
+#include <queue>
 #include <vector>
 
 class BipartiteGraph {
     std::size_t n;
     std::vector<std::vector<int>> graph;
+    std::vector<bool> side;
 
     bool findAugmentingPath(int, std::vector<int>&, std::vector<bool>&);
 
 public:
-    BipartiteGraph(std::size_t);
+    BipartiteGraph(std::size_t, std::vector<bool>&);
 
     void addEdge(int, int);
     std::vector<int> findMatching();
+    std::vector<bool> findVertexCover();
 };
 
 

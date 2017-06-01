9568bf4a6440e005faacac6bcdbf4e571edbb0e5
Krzysztof Maziarz <krzysztof.s.maziarz@gmail.com>
2017-06-01 15:17:03 +0200

    Fix minor bug

--- Graph.cpp
+++ Graph.cpp
@@ -1,7 +1,7 @@
 #include "BipartiteGraph.h"
 #include "Graph.h"
 
-Graph::Graph(std::size_t n): n(n) {}
+Graph::Graph(std::size_t n): n(n), graph(n) {}
 
 void Graph::addEdge(int u, int v) {
     graph[u].push_back(v);

4fec268aeca5aebbfd0275705649365a5cdc55e2
Krzysztof Maziarz <krzysztof.s.maziarz@gmail.com>
2017-06-01 14:51:32 +0200

    Implement main vertex cover subroutine

--- Graph.cpp
+++ Graph.cpp
@@ -38,6 +38,98 @@ std::vector<int> solveHalfIntegralLinearProgramming(std::vector<std::vector<int>
     return lpSolution;
 }
 
+void setRemoved(int v, bool setTo, std::vector<bool>& removed, std::vector<int>& degree,
+                std::vector<std::vector<int>>& graph, std::set<std::pair<int,int>>& vertices) {
+    removed[v] = setTo;
+
+    if (setTo) {
+        vertices.erase({degree[v], v});
+    } else {
+        vertices.insert({degree[v], v});
+    }
+
+    for (int u : graph[v]) {
+        int newDeg = degree[u] + (setTo ? -1 : 1);
+
+        if (!removed[u]) {
+            vertices.erase({degree[u], u});
+            vertices.insert({newDeg, u});
+        }
+
+        degree[u] = newDeg;
+    }
+}
+
+void updateCover(std::vector<bool>& currentCover, std::vector<bool>& bestCover) {
+    auto countTrue = [](auto& vec) { return std::count(vec.begin(), vec.end(), true); };
+
+    if (countTrue(currentCover) < countTrue(bestCover)) {
+        bestCover = currentCover;
+    }
+}
+
+void Graph::subgraphVertexCover(bool useLinearProgramming, std::vector<bool>& taken, std::vector<bool>& removed,
+                                std::vector<int>& degree, std::set<std::pair<int, int>>& vertices,
+                                std::vector<bool>& bestCover) {
+    std::vector<int> verticesToRestore;
+
+    auto removeVertex = [&](int v) {
+        setRemoved(v, true, removed, degree, graph, vertices);
+        verticesToRestore.push_back(v);
+    };
+
+    if (useLinearProgramming) {
+        auto lpSolution = solveHalfIntegralLinearProgramming(graph, removed);
+
+        for (int v = 0; v < n; v++) {
+            if (!removed[v] && lpSolution[v] != 1) {
+                bool take = lpSolution[v] == 2;
+
+                taken[v] = take;
+                removeVertex(v);
+            }
+        }
+    }
+
+    if (vertices.empty()) {
+        updateCover(taken, bestCover);
+    } else if (vertices.begin()->first <= 1) {
+        int v = vertices.begin()->second;
+
+        taken[v] = false;
+        removeVertex(v);
+
+        if (degree[v] == 1) {
+            int u = *std::find_if(graph[v].begin(), graph[v].end(), [&](int i) -> bool { return !removed[i]; });
+
+            taken[u] = true;
+            removeVertex(u);
+        }
+
+        subgraphVertexCover(useLinearProgramming, taken, removed, degree, vertices, bestCover);
+    } else {
+        int v = vertices.rbegin()->second;
+        removeVertex(v);
+
+        taken[v] = true;
+        subgraphVertexCover(useLinearProgramming, taken, removed, degree, vertices, bestCover);
+
+        taken[v] = false;
+        for (int u : graph[v]) {
+            if (!removed[u]) {
+                taken[u] = true;
+                removeVertex(u);
+            }
+        }
+
+        subgraphVertexCover(useLinearProgramming, taken, removed, degree, vertices, bestCover);
+    }
+
+    for (int i : verticesToRestore) {
+        setRemoved(i, false, removed, degree, graph, vertices);
+    }
+}
+
 std::vector<bool> Graph::findVertexCover(bool useLinearProgramming) {
     return std::vector<bool>(); // TODO
 }
--- Graph.h
+++ Graph.h
@@ -1,11 +1,16 @@
 #ifndef VERTEX_COVER_GRAPH_H
 #define VERTEX_COVER_GRAPH_H
 
+#include <algorithm>
 #include <vector>
+#include <set>
 
 class Graph {
     std::size_t n;
     std::vector<std::vector<int>> graph;
+
+    void subgraphVertexCover(bool, std::vector<bool>&, std::vector<bool>&, std::vector<int>&,
+                             std::set<std::pair<int,int>>&, std::vector<bool>&);
 
 public:
     Graph(std::size_t);

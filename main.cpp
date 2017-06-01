50367e4a196c0f6891a98a858017207e134ee13e
Krzysztof Maziarz <krzysztof.s.maziarz@gmail.com>
2017-06-01 22:31:16 +0200

    Add basic graph parsing from stdin

--- main.cpp
+++ main.cpp
@@ -1,3 +1,37 @@
+#include "Graph.h"
+#include <iostream>
+
+Graph readGraph(std::istream& stream = std::cin) {
+    std::size_t  n;
+    stream >> n;
+
+    Graph graph(n);
+
+    int m;
+    stream >> m;
+
+    while (m--) {
+        int u, v;
+        stream >> u >> v;
+
+        graph.addEdge(u, v);
+    }
+
+    return graph;
+}
+
 int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    Graph graph = readGraph();
+    auto vertexCover = graph.findVertexCover(true);
+
+    std::cout << std::count(vertexCover.begin(), vertexCover.end(), true) << '\n';
+    for (int i = 0; i < vertexCover.size(); i++) {
+        if (vertexCover[i]) {
+            std::cout << i << ' ';
+        }
+    }
+
     return 0;
 }
